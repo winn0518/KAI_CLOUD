3449411f522375c9b0b89eaebf2704d68c125705
w2i5n0n9 <23022009@myrp.edu.sg>
2026-01-31 23:43:05 +0800

    change

--- app.py
+++ app.py
@@ -620,5 +620,8 @@ def ask_question():
 
 
 import os
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
